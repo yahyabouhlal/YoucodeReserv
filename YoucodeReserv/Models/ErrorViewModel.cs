c67b4a8aafb0cef86ab377ef03566d54a3e8f173
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2021-03-09 16:31:54 +0100

    Install-pakage

---
+++ YoucodeReserv/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace YoucodeReserv.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
