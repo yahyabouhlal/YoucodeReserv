d70bd207643518951ab6d1e3f40b225292bd5211
yahyabouhlal <yahya.bouhlal.b@gmail.com>
2021-03-11 11:41:10 +0100

    Add-reservation

---
+++ YoucodeReserv/Data/Resrvation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YoucodeReserv.Data
+{
+    public class Resrvation
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("RequestingStudentId")]
+        public Etudiants RequestingStudent { get; set; }
+        public string RequestingStudentId { get; set; }
+        [ForeignKey("ReservationTypeId")]
+        public TypeReservation ReservationType { get; set; }
+        public int ReservationTypeId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
